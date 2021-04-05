b19e1e23125334b854033d1d1ea2146ff6904a34
Yanet Rojo <yanetrojo01@gmail.com>
2021-04-05 17:42:16 -0500

    Carrusel(h3lp)
    - se editó Navbar un poco
    - se agregó un carrusel (aún en proceso)

--- angry-games/src/App.jsx
+++ angry-games/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import GameList from './components/GameList/GamesList';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import Carousel from './components/CarouselHome';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Carousel />
       <div className="App">
         <GameList />        
       </div>
--- angry-games/src/components/Navbar.jsx
+++ angry-games/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import './Navbar.css';
 
-export default function GamesList() {
+export default function NavGames() {
     return(
         <Navbar className="Nav">
             <Navbar.Brand className="Menu" href="#home">Angry Games</Navbar.Brand>
