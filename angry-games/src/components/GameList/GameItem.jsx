8e1b7a0babee118e0c27f3bcd156ebad57f11fc5
Francisco Salazar <fran.pacorro2001@gmail.com>
2021-04-04 23:33:48 -0500

    tarjetas de juegos

--- angry-games/src/components/GameList/GameItem.jsx
+++ angry-games/src/components/GameList/GameItem.jsx
@@ -1,12 +1,26 @@
-import react from 'react';
-import { ListGroup } from 'react-bootstrap';
+import React from 'react';
+import { ListGroup, Card, ListGroupItem } from 'react-bootstrap';
 
 export default function GameItem(props){
     const game = props.game;
 
     return (
         <ListGroup.Item>
-           <label> {game.id} {game.title} </label>
+            <Card>
+                <Card.Img variant="top" height="100 rem" width="auto" src="https://i.stack.imgur.com/y9DpT.jpg"/>
+                <Card.Body>
+                    <Card.Title>{game.title}</Card.Title>
+                    <Card.Text>
+                    {game.description}
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem>Categoría: {game.category}</ListGroupItem>
+                  <ListGroupItem>Desarrollador: {game.developer}</ListGroupItem>
+                  <ListGroupItem>Fecha de lanzamiento: {game.releaseDate}</ListGroupItem>
+                </ListGroup>
+            </Card>
+            <br />
         </ListGroup.Item>
     );
 }
