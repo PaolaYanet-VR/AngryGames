96b614991b2eb6995ea7630eb4dfaff53120de2e
Yare <yareram78@gmail.com>
2021-04-07 22:57:36 -0500

    URL de las imagenes

--- angry-games/src/requests/ConsultGamesRequest.js
+++ angry-games/src/requests/ConsultGamesRequest.js
@@ -3,9 +3,9 @@ import Game from '../representations/Game';
 export default class ConsultGamesRequest {
     execute(){
         return[
-            new Game(1, 'title1', 'developer', 'image', 'description', 'releaseDate', 'category'),
-            new Game(2, 'title2', 'developer', 'image', 'description', 'releaseDate', 'category'),
-            new Game(3, 'title3', 'developer', 'image', 'description', 'releaseDate', 'category')
+            new Game(1, 'title1', 'developer', 'https://fress.co/wp-content/uploads/2014/05/fenomenos-naturales-19.jpg', 'description', 'releaseDate', 'category'),
+            new Game(2, 'title2', 'developer', 'https://tse4.mm.bing.net/th?id=OIP.YGj4e_2KHVNMEOV1ci4kdgHaEn&pid=Api', 'description', 'releaseDate', 'category'),
+            new Game(3, 'title3', 'developer', 'https://tse1.mm.bing.net/th?id=OIP.YW8de0GWZBBRKTxYtsyk0wHaFj&pid=Api', 'description', 'releaseDate', 'category')
         ];
     }
 }
