b5227a64b8475eb8b48e1931121a4644e54eb9af
Yanet Rojo <yanetrojo01@gmail.com>
2021-04-07 23:27:53 -0500

    Links de nav

--- angry-games/src/components/Navbar.jsx
+++ angry-games/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import './Navbar.css';
 
 export default function NavGames() {
     return(
         <Navbar className="Nav">
             <Navbar.Brand className="Menu" href="#home">Angry Games</Navbar.Brand>
+            <Nav.Link className="link" href="#home">Catálogo</Nav.Link>
+            <Nav.Link className="link" href="#link">Agregar</Nav.Link>
         </Navbar>
     );
 }
