9fba7f99f242de4cc0b912cf4d2a2b8412e12094
Yanet Rojo <yanetrojo01@gmail.com>
2021-04-07 21:41:44 -0500

    margin-top again

--- angry-games/src/components/CarouselHome.jsx
+++ angry-games/src/components/CarouselHome.jsx
@@ -7,7 +7,7 @@ export default function CarouselHome() {
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=First slide&bg=373940"
+                src="https://i.redd.it/i9iqra3s5y111.jpg"
                 alt="First slide"
                 />
                 <Carousel.Caption>
@@ -18,7 +18,7 @@ export default function CarouselHome() {
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=Second slide&bg=282c34"
+                src="https://i.redd.it/i9iqra3s5y111.jpg"
                 alt="Second slide"
                 />
 
@@ -30,7 +30,7 @@ export default function CarouselHome() {
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=Third slide&bg=20232a"
+                src="https://i.redd.it/i9iqra3s5y111.jpg"
                 alt="Third slide"
                 />
 
