03c048903c62e07fd3f325692ee9c9e6001c5392
Yanet Rojo <yanetrojo01@gmail.com>
2021-04-08 01:16:33 -0500

    Logo
    si

--- angry-games/src/components/Router/Navbar.js
+++ angry-games/src/components/Router/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import './Navbar.css';
+import logo from '../../img/LogoPre.png';
 
 export default function NavGames() {
     return(
         <Navbar className="Nav">
-            <Navbar.Brand className="Menu" href="#home">Angry Games</Navbar.Brand>
+            <Navbar.Brand className="Menu" href="#home"><img className="logo" src={logo}></img>Angry Games</Navbar.Brand>
             <Nav.Link className="link" href="#home">Home</Nav.Link>
             <Nav.Link className="link" href="#link">Add Game</Nav.Link>
         </Navbar>
