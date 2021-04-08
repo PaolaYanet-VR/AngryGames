335a4c7a34b61e7a83f723a0568d8839ff78687d
Yanet Rojo <yanetrojo01@gmail.com>
2021-04-08 00:21:36 -0500

    Router
    agregue lo del router

--- angry-games/src/App.jsx
+++ angry-games/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import GameList from './components/GameList/GamesList';
-import Navbar from './components/Navbar';
+import Navbar from './components/Router/Navbar';
 import Carousel from './components/GameList/CarouselHome';
 
 function App() {
