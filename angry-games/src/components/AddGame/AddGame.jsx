3a241c11f897c866f391c16a7ac790b775f35d0f
Yanet Rojo <yanetrojo01@gmail.com>
2021-04-14 20:46:44 -0500

    Avance del front para addGame
    Avance del front para addGame con inicio de diseño

--- angry-games/src/components/AddGame/AddGame.jsx
+++ angry-games/src/components/AddGame/AddGame.jsx
@@ -1,7 +1,24 @@
+import { Form, Button } from 'react-bootstrap';
+import './AddGame.css';
+
 export default function AddGame(){
     return(
         <div>
-            sí se mostró
+            <Form>
+                <Form.Group>
+                    <Form.File label="Select Image" />
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control className="Input-txt" placeholder="Title" />
+                    <Form.Label>Developer</Form.Label>
+                    <Form.Control className="Input-txt" placeholder="Developer" />
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control className="Input-txt" placeholder="Description" />
+                </Form.Group>
+
+                <Button className="ButtonAdd" type="submit">
+                    Add Game
+                </Button>
+            </Form>
         </div>
     );
 }
