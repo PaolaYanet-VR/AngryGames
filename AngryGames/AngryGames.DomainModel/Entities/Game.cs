48107132256db811a93ce8b5f13d839d725a106a
Francisco Salazar <fran.pacorro2001@gmail.com>
2021-04-23 19:44:05 -0500

    Proyecto DomainModel agregado
    se agregó una biblioteca de clases, dentro una carpeta Entities y la clase Game

---
+++ AngryGames/AngryGames.DomainModel/Entities/Game.cs
@@ -0,0 +1,22 @@
+﻿namespace AngryGames.DomainModel.Entities
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Developer { get; set; }
+        public string Description { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Category { get; set; }
+
+        public Game(int id, string title, string developer, string description, string releaseDate, string category)
+        {
+            Id = id;
+            Title = title;
+            Developer = developer;
+            Description = description;
+            ReleaseDate = releaseDate;
+            Category = category;
+        }
+    }
+}
