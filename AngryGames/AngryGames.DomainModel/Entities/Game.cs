fc57ad002c580ae3ba7070fa3d208488f44ea767
Yanet Rojo <yanetrojo01@gmail.com>
2021-04-23 21:52:44 -0500

    BD v2
    tabla actualizada con sus respectivos procedimientos almacenados

--- AngryGames/AngryGames.DomainModel/Entities/Game.cs
+++ AngryGames/AngryGames.DomainModel/Entities/Game.cs
@@ -5,15 +5,17 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Developer { get; set; }
+        public string Image { get; set; }
         public string Description { get; set; }
         public string ReleaseDate { get; set; }
         public string Category { get; set; }
 
-        public Game(int id, string title, string developer, string description, string releaseDate, string category)
+        public Game(int id, string title, string developer, string image, string description, string releaseDate, string category)
         {
             Id = id;
             Title = title;
             Developer = developer;
+            Image = image;
             Description = description;
             ReleaseDate = releaseDate;
             Category = category;
