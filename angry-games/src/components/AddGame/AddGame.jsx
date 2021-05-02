104e630aa71c7780c4dc9f8d92776e3d9a61639f
IvanCastillo-F <alex9012345678@gmail.com>
2021-05-01 22:24:27 -0500

    GameDetail and GameAdd
    Se le agrego css al GameAdd y se le dio un formulario al GameDetail

--- angry-games/src/components/AddGame/AddGame.jsx
+++ angry-games/src/components/AddGame/AddGame.jsx
@@ -11,24 +11,33 @@ export default function AddGame(){
                     <br />
                     <Row>
                         <Col>
-                            <img className="gameImage" src={gameImage}></img>
-                            <Form.File label="Select Image" />
-                            <Form.Label>Description</Form.Label>
-                            <textarea
-                                className="form-control"
+                            <div className="inline-svg"><img className="gameImage" src={gameImage}></img></div>
+                            <br />
+                            <Form.File   className="neon" label="Select Image" />
+                            <br />
+                            <Form.Label className="neon">Description</Form.Label>
+                            <br />
+                            <textarea 
+                                className="form-control Input-txt"
                                 rows="5"
                                 placeholder="Description"
                             />
                         </Col>
                         <Col>
-                            <Form.Label>Title</Form.Label>
+                            <Form.Label  className="neon">Title</Form.Label>
+                            <br />
                             <Form.Control className="Input-txt" placeholder="Title" />
-                            <Form.Label>Developer</Form.Label>
+                            <br />
+                            <Form.Label  className="neon">Developer</Form.Label>
+                            <br />
                             <Form.Control className="Input-txt" placeholder="Developer" />
                             <br />
-                            <Form.Control type="date" name='date_of_birth' />
+                            <Form.Label  className="neon">Relase Date</Form.Label>
                             <br />
-                            <Form.Control as="select">
+                            <Form.Control className="Input-txt" type="date" name='date_of_birth' />
+                            <br />
+                            <Form.Label  className="neon">Category</Form.Label>
+                            <Form.Control className="Input-txt" as="select">
                                 <option>Default select</option>
                             </Form.Control>
                             <br />
