fc6c7a1f9abc0e9f6d932dac37d3b2e1f86d6d2f
Yanet Rojo <yanetrojo01@gmail.com>
2021-05-03 00:00:17 -0500

    Router arreglado
    se corrigieron errores del router

--- angry-games/src/components/Router/Navbar.js
+++ angry-games/src/components/Router/Navbar.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import './Navbar.css';
 import logo from '../../img/Logov2.png';
+import { Link } from 'react-router-dom';
 
 export default function NavGames() {
     return(
         <Navbar className="Nav">
-            <Navbar.Brand className="Menu" href="home"><img className="logo" src={logo}></img>Angry Games</Navbar.Brand>
-            <Nav.Link className="link" href="home">Home</Nav.Link>
-            <Nav.Link className="link" href="addGame">Add Game</Nav.Link>
+            <Navbar.Brand className="Menu" as={Link} to="/home"><img className="logo" src={logo}></img>Angry Games</Navbar.Brand>
+            <Nav.Link className="link" as={Link} to="/home">Home</Nav.Link>
+            <Nav.Link className="link" as={Link} to="/addGame">Add Game</Nav.Link>
         </Navbar>
     );
 }
--- angry-games/src/components/Router/Router.js
+++ angry-games/src/components/Router/Router.js
@@ -2,13 +2,15 @@ import {
     BrowserRouter,
     Switch,
     Route,
-    Redirect
+    Redirect,
+    useParams
   } from 'react-router-dom';
   import GamesPage from '../../Pages/GamesPage';
   import AddGamePage from '../../Pages/AddGamePage';
   import GameDetailPage from '../../Pages/GameDetailPage';
   import TopNavbar from './Navbar';
   import './Router.css'
+
   
   export default function Router() {
     return (
@@ -19,7 +21,7 @@ import {
             <Switch>
               <Route path="/home" component={GamesPage} />
               <Route path="/addGame" component={AddGamePage} />
-              <Route path="/gameDetail" component={GameDetailPage} />
+              <Route path="/gameDetail/:id" component={GameDetailPage} />
               <Redirect exact path="/" to="/home" />
             </Switch>
           </div>
